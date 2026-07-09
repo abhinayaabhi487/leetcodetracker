7289f9bdab6e18539aef886e928d090e5834cb67
abhinayaabhi487 <abhinayaabhi487@gmail.com>
2026-07-09 09:15:56 +0530

    Create 344-ReverseString

---
+++ 344-ReverseString/344-ReverseString.java
@@ -0,0 +1,13 @@
+// Last updated: 7/9/2026, 9:15:55 AM
+class Solution {
+    public void reverseString(char[] s) {
+        Stack<Character> st = new Stack<>();
+
+        for(char c: s){
+            st.push(c);
+        }
+        for (int i = 0; i < s.length; i++) {
+            s[i] = st.pop();
+        }
+    }
+}
