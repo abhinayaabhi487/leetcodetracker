fdcce950189aa8b800c1afaaca9f95be10c50231
abhinayaabhi487 <abhinayaabhi487@gmail.com>
2026-07-09 09:17:32 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -0,0 +1,11 @@
+// Last updated: 7/9/2026, 9:17:32 AM
+class Solution {
+    public boolean isPalindrome(int x) {
+        String s = String.valueOf(x); 
+        int n = s.length();
+        for (int i=0; i<n/2; i++) {
+            if (s.charAt(i) != s.charAt(n-i-1)) return false;
+        }
+        return true;
+    }
+}
